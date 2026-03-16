3fc9f3f36f9e02868fae58162f70e619ee7548d9
Amresh2004 <amreshwarad8470@gmail.com>
2026-03-16 12:08:59 +0530

    updte

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
-import
+import { Routes,Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Courses from './pages/Courses';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
