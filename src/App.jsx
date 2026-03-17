21b52d5538a63507bc47a451cacfc7fa2177fed6
Janhavi More <morejanhavi583@gmail.com>
2026-03-17 16:03:40 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,7 @@ import { Routes,Route } from 'react-router-dom'
 import Navbar from './components/landing/Navbar'
 import Footer from './components/landing/Footer';
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Courses from './pages/Courses';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Career from './pages/Career';
+
 import CourseDetails from "./CourseDetails";
 import Home from './pages/landing/Home';
 import About from './pages/landing/About';
