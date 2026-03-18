c363a77317cbdadb053c1ee9f2e64e0088c1eaf0
vidya bhaguji badhale <vidyabadhale2004@gmail.com>
2026-03-18 12:26:04 +0530

    COMMIT

--- src/pages/landing/Career.jsx
+++ src/pages/landing/Career.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import heroImg from "../assets/hero.png";
+
 import { FaArrowRight } from "react-icons/fa";
 
 
 import heroImg from "../../assets/hero.png";
-import counsellingImg from "../../assets/counselling.png";
 
 
 function Career() {
