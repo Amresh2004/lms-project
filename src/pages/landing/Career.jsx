938e6af78c73239d308f0447f0c8d1457843b854
Janhavi More <morejanhavi583@gmail.com>
2026-03-18 12:19:01 +0530

    commit

--- src/pages/landing/Career.jsx
+++ src/pages/landing/Career.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import heroImg from "../../assets/hero.png";
 import { FaArrowRight } from "react-icons/fa";
 
-import { FaArrowRight } from "react-icons/fa";
 
 
-import heroImg from "../../assets/hero.png";
+
+
 
 
 
