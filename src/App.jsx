38acb159f4b30508c050ff0522167167d1b59cd0
Janhavi More <morejanhavi583@gmail.com>
2026-03-23 15:36:17 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Career from "./pages/landing/Career";
 import CourseDetails from "./pages/landing/CourseDetails";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import FacultyDashboard from "./pages/faculty/FacultyDashboard";
+import Sidebar from "./component/Sidebar";
 import StudentDashboard from "./pages/student/StudentDashboard";
 
 import Dashboard from "./pages/admin/Dashboard";
@@ -53,12 +54,25 @@ function App() {
           <Route path="settings" element={<Settings />} />
         </Route>
 
-        <Route path="/faculty/dashboard" element={<FacultyDashboard />} />
-        <Route path="/student/dashboard" element={<StudentDashboard />} />
-      </Routes>
+        <Route path="/faculty/dashboard" element={<FacultyDashboard />}></Route>
 
-      {!hideLayout && <Footer />}
-    </div>
+        <Route path="/dashboard" element={<FacultyDashboard />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/courses" element={<MyCourses />} />
+        <Route path="/assignments" element={<Assignments />} />
+        <Route path="/attendance" element={<Attendance />} />
+        <Route path="/announcements" element={<Announcements />} />
+        <Route path="/upload" element={<UploadMaterials />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/grades" element={<Grades />}>
+</Route>
+      
+
+      <Route path="/student/dashboard" element={<StudentDashboard />} />
+    </Routes>
+
+      { !hideLayout && <Footer /> }
+    </div >
   );
 }
 
