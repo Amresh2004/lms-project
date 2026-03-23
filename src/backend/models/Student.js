8eb1c25ab221cd71a4f302fc5e26f114aa740d22
karanmali490 <karanmali490@gmail.com>
2026-03-23 15:44:13 +0530

    done

--- src/backend/models/Student.js
+++ src/backend/models/Student.js
@@ -1,18 +1,3 @@
-
-const mongoose = require("mongoose");
-
-const studentSchema = new mongoose.Schema({
-  name: String,
-  userId: String,
-  course: String,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-module.exports = mongoose.model("Student", studentSchema);
-
 import mongoose from "mongoose";
 
 const studentSchema = new mongoose.Schema({
